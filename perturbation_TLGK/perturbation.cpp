c11f36127430542aacaa3e36427494ba33be841e
tlestang <thibault.lestang@ens-lyon.fr>
2016-02-12 09:52:29 +0100

    INTRODUCES perturbation module. Initial velocity field is perturbated prior to time evolution. All pertrubation routines stored in separate folder perturbation. Method for generating a gaussian random field (here the stream function) is the Circuland Embedding of The Covariance Matrix. OK for compilation. Execution NOT YET VALIDATED.

---
+++ perturbation_TLGK/perturbation.cpp
@@ -0,0 +1,53 @@
+#ifndef __global__
+#define __global__
+#include "../src/global.h"
+#endif
+
+#include "perturbation.h"
+
+int perturbPopulationField(int n, double *f)
+{
+  double *psi, *du, *dv;
+  double u,v,rho;
+  double eueu, eu, u2, ftemp;
+  double eps = 1.0e-2;
+  int error;
+  psi = new double[n*n];
+  du = new double[n*n];
+  dv = new double[n*n];
+  
+  generate_random_field(n, psi, error);
+  take_curl(n,psi,du,dv,error);
+
+  for(int x=0;x<n;x++)
+    {
+      for(int y=0;y<n;y++)
+	{
+	  rho = 0.0;
+	  u = v = 0.0;
+	  for (int k=0;k<9;k++)
+	    {
+	      ftemp = f[IDX(x,y,k)];
+	      rho += ftemp;
+	      u += ftemp*c[k][0];
+	      v += ftemp*c[k][1];
+	    }
+	  u = u/rho; v = v/rho;
+	  u += eps*du[idx(x,y)]; v += eps*dv[idx(x,y)];
+	  u2 = -1.5*(u*u + v*v);
+	  for(int k=0;k<9;k++)
+	    {
+	      eu = c[k][0]*u + c[k][1]*v;
+	      eueu = 4.5*eu*eu;
+	      f[IDX(x,y,k)] = w[k]*rho*(1.0+3.0*eu+eueu+u2);
+	    }
+	}
+    }
+
+  delete[] psi; delete[] du; delete[] dv;
+}
+	  
+				    
+  
+
+  
---
+++ perturbation_TLGK/perturbation.h
@@ -0,0 +1,3 @@
+int perturbPopulationField(int n, double *f);
+void generate_random_field(int n, double* map_, int &error);
+void take_curl(int n, double* psi, double* curlx, double* curly, int &error);
