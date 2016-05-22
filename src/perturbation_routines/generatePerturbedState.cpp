6254e850d63be44c7731f0850bbb8a548148278a
tlestang <thibault.lestang@ens-lyon.fr>
2016-05-22 14:48:03 +0200

    Modifies definition of the perturbation on the populations. Variable eps is now the imposed value of |\delta u| / |u|

--- src/perturbation_routines/generatePerturbedState.cpp
+++ src/perturbation_routines/generatePerturbedState.cpp
@@ -4,8 +4,8 @@
 int generatePerturbedState(double *fin, double **popsForPerturb, double eps, int NN, int N)
   
 {
-  double *fprim; double norm, randomPrefactor;
-  double oneOvNorm, oneOvA, a;
+  double *fprim; double norm_0, norm_prim, randomPrefactor;
+  double oneOvA, a, weight;
   int error = 0;
   fprim = new double[N];
   double sum_prefactors = 0.0;
@@ -18,18 +18,20 @@ int generatePerturbedState(double *fin, double **popsForPerturb, double eps, int
   	  fprim[i] += randomPrefactor * popsForPerturb[nn][i];
   	}
     }
-  norm = 0.0;
+  norm_0 = 0.0; norm_prim = 0.0;
   for(int i=0;i<N;i++)
     {
-      norm += fprim[i]*fprim[i];
+      norm_0 += fin[i]*fin[i];
+      norm_prim += fprim[i]*fprim[i];
     }
-  norm = sqrt(norm);
-  oneOvNorm = 1./norm;
-  a = 1.0 + sum_prefactors*eps*oneOvNorm;
+  norm_0 = sqrt(norm_0); norm_prim = sqrt(norm_prim);
+  weight = norm_0 / norm_prim;
+  a = 1.0 + sum_prefactors*eps*weight;
   oneOvA = 1./a;
   for(int i=0;i<N;i++)
     {
-      fin[i] += eps*fprim[i]*oneOvNorm*oneOvA;
+      fin[i] += eps*fprim[i]*weight;
+      fin[i] = fin[i]*oneOvA;
     }
   delete[] fprim;
 }
