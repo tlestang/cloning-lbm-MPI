a93e9bd39ae09c5966f6ac6fff8eff833abcdc60
tlestang <thibault.lestang@ens-lyon.fr>
2016-10-10 11:12:01 +0200

    Changed the way force is written on disk. eqch process send the data of its clone to MASTER. MASTER writes the data on disk.

--- 2_sq/launch.sh
+++ 2_sq/launch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for k in 0.1 0.2 0.5
+for k in 0.1 
 do
     
     folderName=k_${k}_$(date +%Y_%m_%d_%H_%M_%S)
@@ -9,5 +9,5 @@ do
     sed "8s/.*/${k}/" input_popsPerturb > ${inputFileName}
     sed -i "10s/.*/${folderName}\//" ${inputFileName}
     
-    mpirun -n 1 ./TLGK_LBM_POPS_PERTURB ${inputFileName}
+    mpirun -n 4 ./TLGK_LBM_POPS_PERTURB ${inputFileName}
 done
